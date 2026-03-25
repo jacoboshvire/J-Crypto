7f1b48b39b82e9ec0a07ba121d84632617442bd2
Ejiro Jacob Oshevire <jacob@unknown.home>
2026-03-25 00:38:40 +0000

    auto commit @Wed Mar 25 00:38:40 GMT 2026

---
+++ src/frequencyAnalysis.js
@@ -0,0 +1,26 @@
+/** @format */
+
+function frequencyAnalysis(text) {
+  const freq = {};
+
+  // Initialize alphabet
+  for (let i = 0; i < 26; i++) {
+    const letter = String.fromCharCode(97 + i);
+    freq[letter] = 0;
+  }
+
+  // Count letters
+  for (let char of text.toLowerCase()) {
+    if (freq.hasOwnProperty(char)) {
+      freq[char]++;
+    }
+  }
+
+  // Convert to sorted array
+  const sorted = Object.entries(freq).sort((a, b) => b[1] - a[1]);
+
+  return {
+    raw: freq,
+    sorted,
+  };
+}
