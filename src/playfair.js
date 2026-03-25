0073ad76abc7f5105701ed194d85daea51b37962
Ejiro Jacob Oshevire <jacob@unknown.home>
2026-03-25 00:42:56 +0000

    auto commit @Wed Mar 25 00:42:56 GMT 2026

--- src/playfair.js
+++ src/playfair.js
@@ -41,3 +41,47 @@ function playfairDecode(text, key = "KEY") {
     .map(([a, b]) => playfairShift(square, a, b, -1).join(""))
     .join("");
 }
+
+module.export={
+    function playfairShift(square, a, b, direction) {
+  const posA = getPosition(square, a);
+  const posB = getPosition(square, b);
+
+  // Same row
+  if (posA.row === posB.row) {
+    return [
+      square[posA.row * 5 + ((posA.col + direction + 5) % 5)],
+      square[posB.row * 5 + ((posB.col + direction + 5) % 5)],
+    ];
+  }
+
+  // Same column
+  if (posA.col === posB.col) {
+    return [
+      square[((posA.row + direction + 5) % 5) * 5 + posA.col],
+      square[((posB.row + direction + 5) % 5) * 5 + posB.col],
+    ];
+  }
+
+  // Rectangle swap
+  return [square[posA.row * 5 + posB.col], square[posB.row * 5 + posA.col]];
+}
+
+function playfairEncode(text, key = "KEY") {
+  const square = generateKeySquare(key);
+  const pairs = prepareText(text);
+
+  return pairs
+    .map(([a, b]) => playfairShift(square, a, b, 1).join(""))
+    .join("");
+}
+
+function playfairDecode(text, key = "KEY") {
+  const square = generateKeySquare(key);
+  const pairs = prepareText(text);
+
+  return pairs
+    .map(([a, b]) => playfairShift(square, a, b, -1).join(""))
+    .join("");
+}
+}
