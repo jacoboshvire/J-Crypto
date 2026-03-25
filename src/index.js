047d3be060580d5f5e1d7b146eae57a272f92992
Ejiro Jacob Oshevire <jacob@unknown.home>
2026-03-25 00:48:13 +0000

    auto commit @Wed Mar 25 00:48:13 GMT 2026

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 
 const { frequencyAnalysis } = require("./frequencyAnalysis");
 const { playfairDecode, playfairEncode } = require("./playfair");
+const { encode, decode, createCipher } = require("./shiftChiper");
 
 module.exports = {
   encode,
